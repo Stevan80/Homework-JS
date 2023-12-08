3d06d58fd71a26c490e4b1c4358a2f2bcd29fb9e
Stevan80 <150294279+Stevan80@users.noreply.github.com>
2023-12-08 11:56:11 +0100

    Add files via upload

---
+++ Variables-JS/app.js
@@ -0,0 +1,8 @@
+let mobilePhones = 30;
+let oneMobilePhone = 119.95;
+let taxForMobile = 5;
+let taxMode = 100;
+let mobilesIncludeTax = oneMobilePhone+oneMobilePhone*taxForMobile/taxMode;
+console.log (mobilesIncludeTax);
+let resultForMobilesPhoneInculdeTax = mobilesIncludeTax*mobilePhones;
+console.log(resultForMobilesPhoneInculdeTax);
