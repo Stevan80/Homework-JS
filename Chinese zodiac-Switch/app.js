81eae2ba7d58ec7bdd5f43450099c96a18e943c4
Stevan80 <150294279+Stevan80@users.noreply.github.com>
2023-12-08 14:37:45 +0100

    Add files via upload

---
+++ Chinese zodiac-Switch/app.js
@@ -0,0 +1,51 @@
+console.log ("Script is a live");
+
+let userInput = prompt("Please enter your year");
+
+let birthYear = Number (userInput);
+
+let chineseZodiac= (birthYear-4)%12;
+
+
+switch (chineseZodiac) {
+    case 0:
+    console.log("Rat");
+    break;
+    case 1:
+    console.log("Ox");
+    break;
+    case 2:
+    console.log("Tiger");
+    break;
+    case 3:
+    console.log("Rabbit");
+    break;
+    case 4:
+    console.log("Dragon");
+    break;
+    case 5:
+    console.log("Snake");
+    break;
+    case 6:
+    console.log("Horse");
+    break;
+    case 7:
+    console.log("Goat");
+    break;
+    case 8:
+    console.log("Monkey");
+    break;
+    case 9:
+    console.log("Rooster");
+    break;
+    case 10:
+    console.log("Dog");
+    break;
+    case 11:
+    console.log("Pig");
+    break;
+    default:
+    console.log("birthYearInvalid");
+    
+       
+} 
