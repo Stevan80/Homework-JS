6652f76f29b42a2975bcc9ca8b6ce24e87110854
Stevan80 <150294279+Stevan80@users.noreply.github.com>
2023-12-10 17:16:20 +0100

    Add files via upload

---
+++ Homework part 1/app.js
@@ -0,0 +1,14 @@
+console.log ("Script is a live");
+
+function fiveDifferentTypes(myAge)
+{
+let dataType = myAge;
+console.log (typeof dataType);
+
+}
+
+fiveDifferentTypes(20);
+fiveDifferentTypes("myAge");
+fiveDifferentTypes(false);
+fiveDifferentTypes(null);
+fiveDifferentTypes();
