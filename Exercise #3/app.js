d0ebd066c2b3f6fb7238a3f492b8f05abfc98876
Stevan80 <150294279+Stevan80@users.noreply.github.com>
2023-12-10 15:01:41 +0100

    Add files via upload

---
+++ Exercise #3/app.js
@@ -0,0 +1,15 @@
+console.log ("Script is a live");
+
+
+function calculateAge(birthYear, currentYear=2023) {
+
+    let yourAge = currentYear - birthYear;
+  
+    console.log("Your age is " + yourAge) ;
+
+  }
+
+  calculateAge (1980);
+  calculateAge (1990);
+  calculateAge (2000);
+  
